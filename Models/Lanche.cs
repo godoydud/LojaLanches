f563a430d4032e89cb4d68cfc5009584a9213dee
Eduardo Godoy Fuso - Estágio <estagiosst14@sesims.com.br>
2021-09-30 07:52:13 -0400

    Adicionando e povoando o BD através das migrations

--- Models/Lanche.cs
+++ Models/Lanche.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,16 @@ namespace LojaLanches.Models
     public class Lanche
     {
         public int LancheId { get; set; } // Identifica como chave primária (utilizada para identificar um item unicamente)
+        [StringLength(100)]
         public string Nome { get; set; }
+        [StringLength(100)]
         public string DescricaoCurta { get; set; }
+        [StringLength(255)]
         public string DescricaoDetalhada { get; set; }
-        public decimal Preco { get; set; }
+        public double Preco { get; set; }
+        [StringLength(200)]
         public string ImagemUrl { get; set; }
+        [StringLength(200)]
         public string ImagemThumbnailUrl { get; set; }
         public bool IsLanchePreferido { get; set; }
         public bool EmEstoque { get; set; }
